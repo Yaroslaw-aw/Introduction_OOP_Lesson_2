3dcd3c8eacea2639445b3652055a886d6fa8cb0d
Yaroslaw-aw <manumission@mail.ru>
2023-08-29 19:46:31 +0300

    update

--- Cats.cs
+++ Cats.cs
@@ -2,26 +2,26 @@
 {
     internal class Cats
     {
-        public List<Cat> cats;
+        public Cat[] cats;
         public int size = 0;
 
         public Cats()
         {
             int random = new Random().Next(5, 15);
 
-            cats = new List<Cat>();
+            cats = new Cat[random];
 
             for (int i = 0; i < random; i++)
             {
                 int appetite = new Random().Next(10, 20);
-                cats.Add(new Cat(string.Format($"Cat{i + 1}"), appetite));
+                cats[i] = new Cat(string.Format($"Cat{i + 1}"), appetite);
                 this.size++;
             }
         }
 
         public void CatsInfo()
         {
-            for (int i = 0; i < cats.Count; i++)
+            for (int i = 0; i < cats.Length; i++)
             {
                 cats[i].CatInfo();
             }
